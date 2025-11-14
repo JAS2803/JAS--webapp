fc9d5b4c2b547f824733d95d4c22c07f98241f39
JAS2803 <jas.0312.2008@gmail.com>
2025-11-14 17:26:04 +0530

    Create admin.js

---
+++ admin.js
@@ -0,0 +1,16 @@
+function uploadFile() {
+const cls = document.getElementById("classSelect").value;
+const sub = document.getElementById("subjectSelect").value;
+const file = document.getElementById("fileInput").files[0];
+
+
+const path = `materials/${cls}/${sub}/${file.name}`;
+
+
+storage.ref(path).put(file).then(() => {
+storage.ref(path).getDownloadURL().then(url => {
+db.collection("files").add({ class: cls, subject: sub, name: file.name, url });
+document.getElementById("uploadMsg").innerText = "Uploaded Successfully!";
+});
+});
+}
